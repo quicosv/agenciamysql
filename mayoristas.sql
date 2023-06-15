58dc5c6f556517057e20d8802ade2648a771a10c
Bilal <mskenmasters@hotmail.com>
2023-06-15 20:11:47 +0200

    corrige error en la linea de Mario García, funciona porque lo he probado xd

--- mayoristas.sql
+++ mayoristas.sql
@@ -1,29 +1,38 @@
--- Inserciones de mayoristas
-insert into
+INSERT INTO
 	mayoristas
-values
-	(Default,
+VALUES
+	(
+		Default,
 		"Agapito Seisluces",
 		"620621622",
 		"Avenida Bonaire 48, planta 36, apto. 72",
-		"Fernando Leal")
-		(Default,
+		"Fernando Leal"
+	),
+	(
+		Default,
 		"Mario García",
 		"688612739",
 		"Avenida Jáudenes 13, planta 6, apto. 7",
-		"Pedro Sánchez")
-		(Default,
-		"Andrés  Bermúdez",
+		"Pedro Sánchez"
+	),
+	(
+		Default,
+		"Andrés Bermúdez",
 		"605153385",
 		"Avenida Argentina 1, planta 2, apto. 1",
-		"Irene Montero")
-		(Default,
-		"Juaquín Guzmán",
+		"Irene Montero"
+	),
+	(
+		Default,
+		"Joaquín Guzmán",
 		"669136862",
 		"Avenida Guadalorce 243, planta 10, apto. 4",
-		"Pablo Iglesias")
-		(Default,
+		"Pablo Iglesias"
+	),
+	(
+		Default,
 		"Patricia Rodríguez",
 		"677665544",
 		"Avenida Diagonal 22, planta 2, apto. 2",
-		"Yolanda Díaz");
+		"Yolanda Díaz"
+	);
