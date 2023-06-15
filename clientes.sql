c94e4518cf9237f04e30e3abdacfc9e34ba04850
gemaarcas <gemaarcas1974@gmail.com>
2023-06-15 21:38:08 +0200

    inserta clientes

---
+++ clientes.sql
@@ -0,0 +1,7 @@
+insert into clientes values
+  (Default, "Carlos García", "Málaga", "952323930"),
+  (Default, "Luisa Ortega", "Granada", "958232581"),
+  (Default, "María Jiménez", "Huelva", "959654321"),
+(Default, "Roberto Chacón", "Córdoba", "957212830"),
+  (Default, "Laura Núñez", "Barcelona", "937225218"),
+  (Default, "Mario Benítez", "Barcelona", 934123456);
