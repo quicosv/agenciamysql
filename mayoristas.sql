1469e6b17e3de2c80bdbbf0b7042f38438f8f174
Bilal <mskenmasters@hotmail.com>
2023-06-15 20:01:07 +0200

    añade parentesis en la insercion de mayoristas

--- mayoristas.sql
+++ mayoristas.sql
@@ -2,30 +2,28 @@
 insert into
 	mayoristas
 values
-	(
-		Default,
+	(Default,
 		"Agapito Seisluces",
 		"620621622",
 		"Avenida Bonaire 48, planta 36, apto. 72",
-		"Fernando Leal",
-		Default,
+		"Fernando Leal")
+		(Default,
 		"Mario García",
 		"688612739",
 		"Avenida Jáudenes 13, planta 6, apto. 7",
-		"Pedro Sánchez",
-		Default,
+		"Pedro Sánchez")
+		(Default,
 		"Andrés  Bermúdez",
 		"605153385",
 		"Avenida Argentina 1, planta 2, apto. 1",
-		"Irene Montero",
-	Default,
+		"Irene Montero")
+		(Default,
 		"Juaquín Guzmán",
 		"669136862",
 		"Avenida Guadalorce 243, planta 10, apto. 4",
-		"Pablo Iglesias",	
-		Default,
+		"Pablo Iglesias")
+		(Default,
 		"Patricia Rodríguez",
 		"677665544",
 		"Avenida Diagonal 22, planta 2, apto. 2",
-		"Yolanda Díaz"
-	);
+		"Yolanda Díaz");
