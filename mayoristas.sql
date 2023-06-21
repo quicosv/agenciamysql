92538f1f1d0050e8c70ae74d7def99d37a636234
Bilal <mskenmasters@hotmail.com>
2023-06-21 12:20:16 +0200

    modificar archivos de inserción para solucionar el misterio de utf8

--- mayoristas.sql
+++ mayoristas.sql
@@ -1,38 +1,38 @@
 INSERT INTO
 	mayoristas
 VALUES
-	(
-		Default,
-		"Agapito Seisluces",
-		"620621622",
-		"Avenida Bonaire 48, planta 36, apto. 72",
-		"Fernando Leal"
+(	
+			Default,
+		'Agapito Seisluces',
+		'620621622',
+		'Avenida Bonaire 48, planta 36, apto. 72',
+		'Fernando Leal'
 	),
 	(
 		Default,
-		"Mario García",
-		"688612739",
-		"Avenida Jáudenes 13, planta 6, apto. 7",
-		"Pedro Sánchez"
+		'Mario García',
+		'688612739',
+		'Avenida Jáudenes 13, planta 6, apto. 7',
+		'Pedro Sánchez'
 	),
 	(
 		Default,
-		"Andrés Bermúdez",
-		"605153385",
-		"Avenida Argentina 1, planta 2, apto. 1",
-		"Irene Montero"
+		'Andrés Bermúdez',
+		'605153385',
+		'Avenida Argentina 1, planta 2, apto. 1',
+		'Irene Montero'
 	),
 	(
 		Default,
-		"Joaquín Guzmán",
-		"669136862",
-		"Avenida Guadalorce 243, planta 10, apto. 4",
-		"Pablo Iglesias"
+		'Joaquín Guzmán',
+		'669136862',
+		'Avenida Guadalorce 243, planta 10, apto. 4',
+		'Pablo Iglesias'
 	),
 	(
 		Default,
-		"Patricia Rodríguez",
-		"677665544",
-		"Avenida Diagonal 22, planta 2, apto. 2",
-		"Yolanda Díaz"
+		'Patricia Rodríguez',
+		'677665544',
+		'Avenida Diagonal 22, planta 2, apto. 2',
+		'Yolanda Díaz'
 	);
