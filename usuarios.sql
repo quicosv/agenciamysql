2290714d63cf21ddf4243b92cf4efdf2cb1d5c50
Quico Saval <quicosv@gmail.com>
2023-07-07 17:20:55 +0200

    Corrige los tipos en la tabla de usuarios

--- usuarios.sql
+++ usuarios.sql
@@ -2,9 +2,9 @@ drop table if exists usuarios;
 
 create table usuarios (
 	idUsuario int unsigned not null auto_increment primary key,
-	email nvarchar(75) not null,
-	password nvarchar(1000) not null,
-	token nvarchar(500) not null,
+	email varchar(75) not null,
+	password varchar(1000) not null,
+	token varchar(500) not null,
 	roles_idRol int unsigned not null,
 	index (roles_idRol),
 	foreign key (roles_idRol) references roles(idRol)
